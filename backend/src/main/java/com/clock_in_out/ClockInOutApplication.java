7262932c032af55c1f8d657cebb2047c62fcef9e
Ivan Medved <medo1902@Medjo-2.local>
2026-02-02 13:03:49 +0000

    Initial commit: Docker and Spring Boot skeleton

---
+++ backend/src/main/java/com/clock_in_out/ClockInOutApplication.java
@@ -0,0 +1,13 @@
+package com.clock_in_out;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClockInOutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClockInOutApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/clock_in_out/ClockInOutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clock_in_out;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClockInOutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
